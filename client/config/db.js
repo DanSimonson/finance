b1864607c7901b588688b651a27e292fef195da9
daniel simonson <dansimonson@mariposaweb.net>
2022-12-15 01:02:19 -0600

    added code

--- client/config/db.js
+++ client/config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    // const connection = await mongoose.connect(process.env.MONGO_URI, {
-    //   useUnifiedTopology: true,
-    //   useNewUrlParser: true,
-    //   useCreateIndex: true,
-    // });
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Mongo connect error: ${error.message}`);
