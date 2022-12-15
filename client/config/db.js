19c2ba98e3f0a1b5ba1294bbb8d5c7775864d78e
daniel simonson <dansimonson@mariposaweb.net>
2022-12-15 01:25:34 -0600

    changed db name

--- client/config/db.js
+++ client/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL, {
+    const connection = await mongoose.connect(process.env.MONGODB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
