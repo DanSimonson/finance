01eac11eb637ab39dd368a9374e48a98eb0a13e3
daniel simonson <dansimonson@mariposaweb.net>
2022-12-15 03:10:49 -0600

    added dotenv and older import/export javascript

--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
-import mongoose from "mongoose";
-
+/**{
+      useUnifiedTopology: true
+      useNewUrlParser: true
+      useCreateIndex: true
+    } */
+// import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const connectDB = async () => {
-    console.log('process.env.MONGODB_URL: ', process.env.MONGODB_URL)
+  console.log("process.env.MONGODB_URL: ", process.env.MONGODB_URL);
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const connection = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Mongo connect error: ${error.message}`);
     process.exit(1);
   }
 };
-export default connectDB;
+module.exports = {
+  connectDB,
+};
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const path = require("path");
 const express = require("express");
-const mongoose = require("mongoose")
-import connectDB from "./config/db.js";
-
-connectDB()
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const { connectDB } = require("./config/db");
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Handle GET requests to /express_backend route
 app.get("/express_backend", (req, res) => {
-    res.send({ message: "EXPRESS CONNECTED TO REACT AND READY TO UPLOAD EXCEL" });
-  });
-  
-  // Have Node serve the files for our built React app
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
-  
-  // All other GET requests not handled before will return our React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  });
-  
+  res.send({ message: "EXPRESS CONNECTED TO REACT AND READY TO UPLOAD EXCEL" });
+});
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 const port = process.env.PORT || 5001;
 
